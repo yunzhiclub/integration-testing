8bc6ab0f0908b16089068c918982f2d6a80b1db2
wujifen <18184455760@163.com>
2023-12-29 20:22:30 +0800

    检查单元测试

--- web/src/app/part/layout/layout.component.spec.ts
+++ web/src/app/part/layout/layout.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
+import {HeaderModule} from "../header/header.module";
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -8,6 +9,9 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HeaderModule
+      ],
       declarations: [ LayoutComponent ]
     })
     .compileComponents();
