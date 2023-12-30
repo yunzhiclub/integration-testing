7c78a8a5ee62ebbd209a529e82d8935cec4d72cb
zhujie <935947831@qq.com>
2023-12-30 14:47:18 +0800

    初始化后台

---
+++ api/src/main/java/com/mengyunzhi/integrationTesting/integrationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.mengyunzhi.integrationTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class integrationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(integrationTestingApplication.class, args);
+	}
+
+}
