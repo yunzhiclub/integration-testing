44f3a53033e191ee26a161b86c0db668fc8e289f
wujifen <18184455760@163.com>
2023-12-30 18:22:08 +0800

    项目管理初步原型

---
+++ web/src/app/project/large-item/large-item.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LargeItemComponent } from './large-item.component';
+
+describe('LargeItemComponent', () => {
+  let component: LargeItemComponent;
+  let fixture: ComponentFixture<LargeItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LargeItemComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LargeItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ web/src/app/project/project-routing.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {ProjectComponent} from "./project/project.component";
+import {LargeItemComponent} from "./large-item/large-item.component";
+import {SmallItemComponent} from "./small-item/small-item.component";
+import {TestComponent} from "./test/test.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProjectComponent
+  },
+  {
+    path: 'test',
+    component: TestComponent
+  },
+  {
+    path: 'largeItem',
+    component: LargeItemComponent
+  },
+  {
+    path: 'smallItem',
+    component: SmallItemComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProjectRoutingModule { }
