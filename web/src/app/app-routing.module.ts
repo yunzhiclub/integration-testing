6beb8dbacf311ab33cf73f9a0613a6094edc3723
wujifen <18184455760@163.com>
2023-12-30 15:43:17 +0800

    登录

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -13,8 +13,11 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
   {
-    path: '',
-    component: LayoutComponent
+    path: 'dashboard',
+    component: LayoutComponent,
+    children: [
+
+    ]
   }
 ]
 
--- web/src/app/auth/login/login.component.ts
+++ web/src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
+import {UserService} from "../../../service/user.service";
+import {Router} from "@angular/router";
 
 /**
  * 登录组件
@@ -17,10 +19,41 @@ export class LoginComponent implements OnInit{
     password: new FormControl('')
   })
 
+  /** 错误信息 */
+  errorInfo: string | undefined;
+  disabled = false;
+
+  /** 提交状态 */
+  submitting = false;
+
+  constructor(private userService: UserService,
+              private router: Router) {
+  }
   ngOnInit(): void {
+    this.errorInfo = '';
+    this.loginForm.valueChanges
+      .subscribe(() => {
+        this.errorInfo = '';
+      });
   }
 
   login(): void{
+    this.disabled = true;
+    const user = {
+      username: this.loginForm.get('username')?.value as string,
+      password: this.loginForm.get('password')?.value as string,
+    };
 
+    this.userService.login(user)
+      .subscribe({
+        next: () => {
+          this.userService.initCurrentLoginUser(() => {
+            this.router.navigateByUrl('dashboard').then();
+          });
+        }, error: (err) => {
+          console.log('发生错误', err);
+          this.errorInfo = '登录失败，请检查您填写的信息是否正确';
+        }
+      });
   }
 }
