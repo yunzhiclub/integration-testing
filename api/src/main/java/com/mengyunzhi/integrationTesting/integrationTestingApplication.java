1d1ff71309718aafe4341714d521f00ac0de52bd
kexiaobin <13927029761@163.com>
2024-01-05 12:35:03 +0800

    配置config

--- api/src/main/java/com/mengyunzhi/integrationTesting/integrationTestingApplication.java
+++ api/src/main/java/com/mengyunzhi/integrationTesting/integrationTestingApplication.java
@@ -1,9 +1,13 @@
 package com.mengyunzhi.integrationTesting;
 
+import com.mengyunzhi.integrationTesting.repository.SoftDeleteRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(value = "com.mengyunzhi.integrationTesting",
+repositoryBaseClass = SoftDeleteRepositoryFactoryBean.class)
 public class integrationTestingApplication {
 
 	public static void main(String[] args) {
