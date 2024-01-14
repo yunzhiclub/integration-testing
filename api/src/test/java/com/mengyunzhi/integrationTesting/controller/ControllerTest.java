b631d750d478b402d1492a1607bdad4d2e068f58
kexiaobin <13927029761@163.com>
2024-01-14 12:54:46 +0800

    用户管理增加单元测试

---
+++ api/src/test/java/com/mengyunzhi/integrationTesting/controller/ControllerTest.java
@@ -0,0 +1,14 @@
+package com.mengyunzhi.integrationTesting.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public abstract class ControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+}
