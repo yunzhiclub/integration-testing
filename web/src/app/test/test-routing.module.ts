02c736e7bda91e9934cab8d7561a51d5128f1c52
zhujie <935947831@qq.com>
2024-01-17 22:38:49 +0800

    测试管理原型

--- web/src/app/test/test-routing.module.ts
+++ web/src/app/test/test-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {IndexComponent} from "./index/index.component";
+import {TestItemComponent} from "./test-item/test-item.component";
 
 const routes: Routes = [
   {
     path: '',
     component: IndexComponent
+  },
+  {
+    path: 'testItem',
+    component: TestItemComponent
   }
 ];
 
