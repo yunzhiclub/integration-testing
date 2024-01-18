f40741c77bcaac523ad841b94d31277d828db7b3
kexiaobin <13927029761@163.com>
2024-01-18 23:56:42 +0800

    重构首页

--- web/src/app/dashboard/dashboard.module.ts
+++ web/src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import {DashboardRoutingModule} from "./dashboard-routing.module";
+import {UserModule} from "../user/user.module";
 
 
 
@@ -9,9 +10,10 @@ import {DashboardRoutingModule} from "./dashboard-routing.module";
   declarations: [
     DashboardComponent
   ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        DashboardRoutingModule,
+        UserModule
+    ]
 })
 export class DashboardModule { }
