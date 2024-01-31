23ef53372fe22695d7a4778aa71bbbaf43a735cc
wujifen <18184455760@163.com>
2024-01-31 15:10:42 +0800

    2

--- web/src/app/test-admin/test-admin.module.ts
+++ web/src/app/test-admin/test-admin.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { TestAdminRoutingModule } from './test-admin-routing.module';
 import { IndexComponent } from './index/index.component';
+import {FormsModule} from "@angular/forms";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { IndexComponent } from './index/index.component';
   ],
   imports: [
     CommonModule,
-    TestAdminRoutingModule
+    TestAdminRoutingModule,
+    FormsModule,
+    MatTooltipModule
   ]
 })
 export class TestAdminModule { }
