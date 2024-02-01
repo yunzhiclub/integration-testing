2fe6b44caefb25e352610869a2c8b36ef6c3df92
kexiaobin <13927029761@163.com>
2024-02-01 14:37:59 +0800

    测试用例clone

--- api/src/main/java/com/mengyunzhi/integrationTesting/repository/TestItemRepository.java
+++ api/src/main/java/com/mengyunzhi/integrationTesting/repository/TestItemRepository.java
@@ -1,11 +1,21 @@
 package com.mengyunzhi.integrationTesting.repository;
 
+import com.mengyunzhi.integrationTesting.entity.TestCase;
 import com.mengyunzhi.integrationTesting.entity.TestItem;
+import com.mengyunzhi.integrationTesting.repository.Specs.TestItemSpecs;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.*;
 
 /**
  * @author kexiaobin
  */
 public interface TestItemRepository extends PagingAndSortingRepository<TestItem, Long>, JpaSpecificationExecutor<TestItem> {
+
+   default List<TestItem> getBelongTestCase(Long testCaseId) {
+       Specification<TestItem> specification = TestItemSpecs.beLongTestItem(testCaseId);
+       return this.findAll(specification);
+    }
 }
