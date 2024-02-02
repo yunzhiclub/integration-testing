5c1482a7a0dd8cafd41c482d8dc67aabb6f31c87
wujifen <18184455760@163.com>
2024-02-03 04:21:36 +0800

    test entity

--- web/src/entity/test.ts
+++ web/src/entity/test.ts
@@ -1,6 +1,20 @@
+import {TestPlan} from "./testPlan";
+import {TestCase} from "./test-case";
+import {Project} from "./project";
+import {User} from "./user";
+
 /**
  * 测试实体
  */
 export interface Test {
   id: number;
+  /*所属测试计划*/
+  testPlan: TestPlan,
+  /*测试人*/
+  testUser: User,
+  /*所属测试用例*/
+  testCase: TestCase[],
+  /*所属项目*/
+  project: Project,
+  status: number
 }
---
+++ web/src/service/test.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {Action, Store} from "@tethys/store";
+import {TestCase} from "../entity/test-case";
+import {Page} from "@yunzhi/ng-common";
+import {Test} from "../entity/test";
+import {Project} from "../entity/project";
+import {Observable, tap} from "rxjs";
+import {Assert} from "@yunzhi/utils";
+import {HttpClient} from "@angular/common/http";
+
+/**
+ * 测试的状态管理
+ */
+interface TestState extends Store<Test>{
+  pageData: Page<Test>;
+  httpParams: { page: number, size: number };
+
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService extends Store<TestState>{
+  static pageData(state: TestState): Page<Test> {
+    return state.pageData;
+  }
+  constructor(private httpClient: HttpClient) {
+    super({
+      pageData: new Page<Test>(),
+      httpParams: {page: 0, size: 0}
+    });
+  }
+
+  /**
+   * 分页数据
+   */
+  @Action()
+  pageAction(payload: {page: number, size: number}): Observable<Page<Test>> {
+    Assert.isNumber(payload.page, 'page不能为空');
+    Assert.isNumber(payload.size, 'size不能为空');
+
+    //获取state快照
+    const state = this.snapshot;
+    state.httpParams = payload;
+
+    return this.httpClient.get<Page<Test>>('/test/page', {params: payload})
+      .pipe(
+        tap(data => {
+          state.pageData = data as Page<Test>;
+          this.next(state);
+        }),
+      );
+  }
+}
