506454d8bde69d77588bd237453d97aa54d959a7
kexiaobin <13927029761@163.com>
2024-02-03 18:29:59 +0800

    1

--- web/src/app/test-plan/add/add.component.ts
+++ web/src/app/test-plan/add/add.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormArray, FormControl, FormGroup} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {CommonService} from "../../../service/common-service";
 import {TestPlanService} from "../../../service/test-plan.service";
 import {TestPlan} from "../../../entity/testPlan";
 import {Project} from "../../../entity/project";
+import {TestCase} from "../../../entity/test-case";
+import {User} from "../../../entity/user";
 
 /**
  * 测试计划add组件
@@ -16,20 +18,47 @@ import {Project} from "../../../entity/project";
 })
 export class AddComponent implements OnInit{
   formGroup: FormGroup;
+  testPlan: TestPlan;
+
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private testPlanService: TestPlanService,
-              private commonService: CommonService) {
+              private commonService: CommonService,) {
   }
 
   ngOnInit(): void {
+    // this.formGroup = new FormGroup({
+    //   project: new FormControl<Project>(null),
+    //   title: new FormControl<string>(''),
+    //   testCase: new FormControl<string>(''),
+    //   testUser: new FormControl<string>(null),
+    // });
+
     this.formGroup = new FormGroup({
       project: new FormControl<Project>(null),
       title: new FormControl<string>(''),
-      testCase: new FormControl<string>(''),
-      testUser: new FormControl<string>(null),
+      tasks: new FormArray([])
+    })
+
+    this.addFormTasks();
+  }
+
+  addFormTasks() {
+    const tasks = new FormGroup({
+      testCase: new FormControl<TestCase[]>(null),
+      testUser: new FormControl<User>(null),
     });
+
+    (this.formGroup.get('tasks') as FormArray).push(tasks);
+  }
+
+  get formTasks() {
+    return (this.formGroup.get('tasks') as FormArray).controls;
+  }
+
+  removeFormField(index: number) {
+    (this.formGroup.get('tasks') as FormArray).removeAt(index);
   }
 
   onClose() {
@@ -37,18 +66,18 @@ export class AddComponent implements OnInit{
   }
 
   onSubmit() {
-    const testPlan = this.formGroup.value as TestPlan;
+    console.log(this.formGroup.value);
 
-    this.testPlanService.addAction(testPlan).subscribe({
-      next: () => {
-        this.commonService.success(() => {
-          this.onClose();
-        }, '添加成功');
-      }, error: (err) => {
-        this.commonService.error(() => {
-          this.onClose();
-        }, '', '添加失败', err);
-      }
-    });
+    // this.testPlanService.addAction(testPlan).subscribe({
+    //   next: () => {
+    //     this.commonService.success(() => {
+    //       this.onClose();
+    //     }, '添加成功');
+    //   }, error: (err) => {
+    //     this.commonService.error(() => {
+    //       this.onClose();
+    //     }, '', '添加失败', err);
+    //   }
+    // });
   }
 }
